d28529077b717a086c28805e7f4a6f97c4754580
LBiedermann <54768086+LBiedermann@users.noreply.github.com>
2022-11-24 13:49:52 +0100

    Project created

---
+++ Code/D-Coder/Source/lib/MidSide.cpp
@@ -0,0 +1,102 @@
+/*
+  ==============================================================================
+
+    MidSide.cpp
+    Created: 24 Nov 2022 1:16:22pm
+    Author:  Lukas
+
+  ==============================================================================
+*/
+
+#include "MidSide.h"
+
+void MidSide::midSideEncode(const float& L, const float& R, float& M, float& S) {
+    M = 0.707f * (L + R);
+    S = 0.707f * (L - R);
+}
+
+void MidSide::midSideDecode(const float& M, const float& S, float& L, float& R) {
+    L = 0.707f * (M + S);
+    R = 0.707f * (M - S);
+}
+
+void MidSide::processStereoWidth(float& L, float& R) {
+    float M = 0.f;
+    float S = 0.f;
+
+    // LR --> MS
+    midSideEncode(L, R, M, S);
+    float x = S;
+    if (midMute)
+        M = 0.f;
+    else {
+        //width --> 0 to 2
+        if (stereoWidth >= 1)
+            M *= (2.f - stereoWidth);
+        //store Sum for RMS
+        sumMid += M * M;
+    }
+
+    if (sideMute)
+        S = 0.f;
+    else {
+        //monomaker
+
+        iirFilter.processSamples(&S, 1);
+
+        //width --> 0 to 2
+        if (stereoWidth <= 1)
+            S *= stereoWidth;
+
+        //store Sum for RMS
+        sumSide += S * S;
+    }
+
+    midSideDecode(M, S, L, R);
+}
+
+void MidSide::processStereoWidth(float* leftChannel, float* rightChannel, const int N) {
+    sumMid = 0.f;
+    sumSide = 0.f;
+
+    for (int n = 0; n < N; n++)
+    {
+        float L = leftChannel[n];
+        float R = rightChannel[n];
+        processStereoWidth(L, R);
+        leftChannel[n] = L;
+        rightChannel[n] = R;
+    }
+
+    calcRMSLevel(N);
+}
+
+void MidSide::processStereoWidth(juce::AudioBuffer<float>& buffer) {
+
+    float* leftChannel = buffer.getWritePointer(0);
+    float* rightChannel = buffer.getWritePointer(1);
+    int N = buffer.getNumSamples();
+    processStereoWidth(leftChannel, rightChannel, N);
+}
+
+void MidSide::calcRMSLevel(int N) {
+
+    rmsLevelMid.skip(N);
+    rmsLevelSide.skip(N);
+    {
+        const auto midValue = juce::Decibels::gainToDecibels(std::sqrt(sumMid / N));
+        if (midValue < rmsLevelMid.getCurrentValue())
+            rmsLevelMid.setTargetValue(midValue);
+        else
+            rmsLevelMid.setCurrentAndTargetValue(midValue);
+    }
+
+    {
+        const auto sideValue = juce::Decibels::gainToDecibels(std::sqrt(sumSide / N));
+        if (sideValue < rmsLevelSide.getCurrentValue())
+            rmsLevelSide.setTargetValue(sideValue);
+        else
+            rmsLevelSide.setCurrentAndTargetValue(sideValue);
+    }
+
+}
---
+++ Code/D-Coder/Source/lib/MidSide.h
@@ -0,0 +1,109 @@
+/*
+  ==============================================================================
+
+    MidSide.h
+    Created: 24 Nov 2022 1:16:22pm
+    Author:  Lukas
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+//#include "EQ.h"
+
+
+#pragma once
+
+class MidSide {
+
+public:
+
+    void processStereoWidth(float& L, float& R);
+    void processStereoWidth(float* leftChannel, float* rightChannel, const int N);
+    void processStereoWidth(juce::AudioBuffer<float>& buffer);
+
+    void midSideEncode(const float& L, const float& R, float& M, float& S);
+    void midSideDecode(const float& M, const float& S, float& L, float& R);
+
+    //void setStereowidthValue(std::atomic<float>* newWidth) {
+    //    strVal.setTargetValue(newWidth->load());
+    //    while (strVal.isSmoothing())
+    //    {
+    //        stereoWidth = strVal.getNextValue();
+    //        //DBG(stereoWidth);
+    //    }
+
+    //}
+
+    void updateCutFilter(std::atomic<float>* newFrequency) {
+
+        //setHpfValue(newFrequency->load());
+        currentHpfValue.setTargetValue(newFrequency->load());
+        while (currentHpfValue.isSmoothing())
+        {
+            iirFilter.setCoefficients(juce::IIRCoefficients::makeHighPass
+            (sampleRate, currentHpfValue.getNextValue()));
+            //DBG(currentHpfValue.getNextValue());
+        }
+    }
+
+    void reset() {
+        iirFilter.reset();
+        rmsLevelMid.reset(sampleRate, 0.5);
+        rmsLevelSide.reset(sampleRate, 0.5);
+
+        rmsLevelMid.setCurrentAndTargetValue(-100.f);
+        rmsLevelSide.setCurrentAndTargetValue(-100.f);
+        sumMid = 0.f,
+            sumSide = 0.f;
+
+        strVal.reset(sampleRate, 0.005);
+        currentHpfValue.reset(sampleRate, 0.005);
+    }
+    void setSamplerate(double newSamplerate) {
+        sampleRate = newSamplerate;
+    }
+
+
+    void calcRMSLevel(int N);
+
+
+    float getRMSLevelMid() {
+        return rmsLevelMid.getCurrentValue();
+    }
+    float getRMSLevelSide() {
+
+        return rmsLevelSide.getCurrentValue();
+    }
+
+    void setMidState(bool midState) {
+        midMute = midState;
+    }
+
+    void setSideState(bool sideState) {
+        sideMute = sideState;
+    }
+
+    //void setHpfValue(float value) {
+
+
+    //    if (value < currentHpfValue.getCurrentValue())
+    //        currentHpfValue.setTargetValue(value);
+    //    else
+    //        currentHpfValue.setCurrentAndTargetValue(value);
+    //}
+
+private:
+
+
+    float stereoWidth = 1.0f;
+
+    double sampleRate = 48000;
+    //float frequenzy = 20.f;
+    juce::IIRFilter iirFilter;
+
+    float sumMid = 0.f, sumSide = 0.f;
+    juce::LinearSmoothedValue<float> rmsLevelMid, rmsLevelSide, strVal, currentHpfValue;
+
+    bool midMute = false, sideMute = false;
+};
